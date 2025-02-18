1237c518be866b810302f2e27b07e3c412e7a899
윤종환 <yunjonghwan99@gmail.com>
2025-02-18 14:54:29 +0900

    modify: 작성한 파일 정렬

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import fs from "fs";
 
 const port = 3000;
 
-const server = http.createServer(function(req, res) {
-  if(req.method === "GET") {
+const server = http.createServer(function (req, res) {
+  if (req.method === "GET") {
     console.log("in GET");
-    if(req.url === "/") {
+    if (req.url === "/") {
       console.log("in GET in mainPageUrl");
       const page = fs.readFileSync("./index.html");
-      res.writeHead(200, {"Content-type" : "utf-8; text/html"});
+      res.writeHead(200, { "Content-type": "utf-8; text/html" });
       res.write(page);
       res.end();
     }
